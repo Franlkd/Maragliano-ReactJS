7d4d84e015c99a43055cd4544462d304408af1d4
Fran89-mara <francomaragliano04@hotmail.com>
2024-11-06 14:00:50 -0300

    Proyecto final

--- vite-project/src/pages/sobreNosotros/sobreNosotros.jsx
+++ vite-project/src/pages/sobreNosotros/sobreNosotros.jsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React from 'react';
 
-export const SobreNosotros = () => {
-
-    return(
-        <div>
-            Sobre nosotros
+const SobreNosotros = () => {
+  return (
+    <section className="sobre-nosotros" id="sobre-nosotros">
+      <div className="container">
+        <h2 className="titulo">Sobre Nosotros</h2>
+        <div className="contenido">
+          <img 
+            src="public/img/equipoDeTrabajo.jpg" 
+            alt="Nuestro Equipo" 
+            className="imagen-equipo"
+          />
+          <div className="texto">
+            <p>
+              En TecnoPC, nos dedicamos a la comercializacion de PCs armadas de marcas como Intel y AMD.
+              Fundada en 2009, nuestro objetivo es la entrega de productos de calidad a un precio relativamente economico y sin perder el rendimiento que el usuario busca. 
+              Contamos con un equipo de profesionales apasionados que se esfuerzan por 
+              ofrecer la atencion y contribuccion de la más alta calidad.
+            </p>
+            <p>
+              Creemos que hay una forma de entregar buen rendimiento a bajos costos, y nos comprometemos a entregar calidad y cudiar su bolsillo.
+              Nuestra misión es llegar a una buena cantidad de clientes satisfechos y trabajamos constantemente para 
+              superar las expectativas de nuestros clientes.
+            </p>
+          </div>
         </div>
-    )
+      </div>
+    </section>
+  );
 }
+
+export default SobreNosotros;
